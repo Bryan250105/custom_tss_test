8e982d4610ea863a9d5e884d1b4be115e4f11cc5
Bryan250105 <josephsoumury@tobatech.id>
2026-09-03 10:15:00 +0700

    Property Setter and Agent Report

--- custom_tss/hooks.py
+++ custom_tss/hooks.py
@@ -256,3 +256,6 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [
+    {"doctype": "Property Setter", "filters": [["module", "=", "Custom TSS"]]},
+]
